dda68347788bff59a45fb650b50981a403defe37
zackgq2009 <zackgq@hotmail.com>
2020-06-02 14:54:39 +0800

    add several parameters in operate_snapshot

--- samples/operate_snapshot.py
+++ samples/operate_snapshot.py
@@ -42,6 +42,14 @@ from tools import cli
 def setup_args():
     parser = argparse.ArgumentParser(
         description='Operation of Snapshot in Virtual Machine.')
+    parser.add_argument('-s', '--host', required=True, action='store',
+                        help='Remote host to connect to')
+    parser.add_argument('-o', '--port', type=int, default=443, action='store',
+                        help='Port to connect on')
+    parser.add_argument('-u', '--user', required=True, action='store',
+                        help='User name to use when connecting to host')
+    parser.add_argument('-p', '--password', required=True, action='store',
+                        help='Password to use when connecting to host')
     parser.add_argument('-vn', '--vm_name', required=True,
                         help="the name of virtual machine.")
     parser.add_argument('-o', '--operation', required=True,
